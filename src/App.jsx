7994864f62650aa3b19b2445e7062dad7a39a53b
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-24 13:18:53 +0300

    header conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import Auth from "./components/Auth";
 import UserProfile from "./components/UserProfile";
 
+// jei vartotojas prisilogines, rodyti UserProfile, COunter ir Header
+// jei ne Header ir Auth
+
 function App() {
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useSelector } from "react-redux";
 
+// kai store isAuthenticated  === false tai neromdom linku ir button sako Login
+// header.jsx paspaudus logout turetu mus isloginti. (paspaudus neturetu priloginti atgal)
 const Header = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isAuthetnticated);
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
+          {isLoggedIn && (
+            <>
+              <li>
+                <a href="/">My Products</a>
+              </li>
+              <li>
+                <a href="/">My Sales</a>
+              </li>
+            </>
+          )}
+
           <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
+            {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
           </li>
         </ul>
       </nav>
