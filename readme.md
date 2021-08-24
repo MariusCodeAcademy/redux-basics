1141187ccac4695310b43e51993ecc71069f93a8
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-24 09:34:18 +0300

    created actions

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # redux basics in react project
+
+1. npm i redux react-redux
+
+2. susikuti redux pagr faila
+
+3. aprasom actions ir state
+
+4. Provide the store
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,23 @@
 import { createStore } from "redux";
 
+const initialCounterState = { counter: 0 };
 // reducer funkcija
-const counterReducer = () => {};
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case "UP":
+      return {
+        counter: state.counter + 1,
+      };
+    case "DOWN":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
 
 // store sukurimas
 const store = createStore(counterReducer);
+
+export default store;
