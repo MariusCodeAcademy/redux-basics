f290a2f1dac5424dbd0812374ee7ad82dc717768
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-24 11:07:03 +0300

    configureStore, counterActions export

--- src/store/index.js
+++ src/store/index.js
@@ -23,38 +23,44 @@ const counterSlice = createSlice({
   },
 });
 
-// reducer funkcija
-const counterReducer = (state = initialCounterState, action) => {
-  switch (action.type) {
-    case "UP":
-      //   state.counter++; // tiesiogiai keiciam state / mutuojam jo reiksme
-      //   return state; // taip NIEKADA NEDARYTI !!!!!!!!!!!
+// // reducer funkcija
+// const counterReducer = (state = initialCounterState, action) => {
+//   switch (action.type) {
+//     case "UP":
+//       //   state.counter++; // tiesiogiai keiciam state / mutuojam jo reiksme
+//       //   return state; // taip NIEKADA NEDARYTI !!!!!!!!!!!
 
-      return {
-        counter: state.counter + 1,
-        showCounter: state.showCounter,
-      };
-    case "DOWN":
-      return {
-        counter: state.counter - 1,
-        showCounter: state.showCounter,
-      };
-    case "UP_BY":
-      return {
-        counter: state.counter + action.amount,
-        showCounter: state.showCounter,
-      };
-    case "TOGGLE":
-      return {
-        counter: state.counter,
-        showCounter: !state.showCounter,
-      };
-    default:
-      return state;
-  }
-};
+//       return {
+//         counter: state.counter + 1,
+//         showCounter: state.showCounter,
+//       };
+//     case "DOWN":
+//       return {
+//         counter: state.counter - 1,
+//         showCounter: state.showCounter,
+//       };
+//     case "UP_BY":
+//       return {
+//         counter: state.counter + action.amount,
+//         showCounter: state.showCounter,
+//       };
+//     case "TOGGLE":
+//       return {
+//         counter: state.counter,
+//         showCounter: !state.showCounter,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+// // store sukurimas
+// const store = createStore(counterReducer);
 
-// store sukurimas
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
+console.log(counterSlice.actions); // visi musu aprasyti veiksmai
+
+export const counterActions = counterSlice.actions;
 export default store;
