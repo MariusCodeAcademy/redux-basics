65671d0d2b914f270147161fb9a496ea54641b0d
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-25 08:08:12 +0300

    logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import classes from "./Header.module.css";
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store/index";
 // kai store isAuthenticated  === false tai neromdom linku ir button sako Login
 // header.jsx paspaudus logout turetu mus isloginti. (paspaudus neturetu priloginti atgal)
 const Header = () => {
+  const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isAuthetnticated);
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -22,7 +26,11 @@ const Header = () => {
           )}
 
           <li>
-            {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
+            {isLoggedIn ? (
+              <button onClick={logoutHandler}>Logout</button>
+            ) : (
+              <button>Login</button>
+            )}
           </li>
         </ul>
       </nav>
